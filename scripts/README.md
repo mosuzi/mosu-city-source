db960be8f57ae348a6a9facb7b0809ed69c772a1
mosuzi <mosu_ouyang@sina.com>
2023-02-06 11:25:12 +0800

    介绍

---
+++ scripts/README.md
@@ -0,0 +1,9 @@
+## scripts
+
+__If you cannot run those scripts, you should change those files's permission and make them executable.__
+
+__For example:__
+
+```bash
+chmod +x ./scripts/*.sh
+```
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
+const repository = 'https://github.com/mosuzi/mosu-city-source'
+
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>莫苏城</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: repository,
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/channels/1071337275868332122/1071337279035015170',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: repository,
   footer: {
-    text: 'Nextra Docs Template',
+    text: <span>MIT {new Date().getFullYear()} © Mosu City | Powered by <a href="https://nextra.site" target="_blank">Nextra</a> Docs</span>,
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – 莫苏城'
+    }
+  }
 }
 
 export default config
